ed4942d70b747343ce7ed9324dd9d614f672f0fd
allesst1rbt <62967469+allesst1rbt@users.noreply.github.com>
2020-06-06 21:25:29 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -5,4 +5,4 @@ echo.
  set /p codigo=Digite o codigo da sala:
  set /p nome=digite seu nome e turma:
  set /p palavra=digite uma palavra de 2 LETRAS:
- node index.js %email% %senha% %codigo% %nome% %palavra%
+ node index.js %email% %senha% %codigo% %palavra% %nome%
